24bc81fbae595c57d256874513a4d73756dca361
Atul <kkmonlee@gmail.com>
2017-06-12 04:02:59 +0100

    Tested with example

--- app/src/main/java/budget/saveit/SaveIt.java
+++ app/src/main/java/budget/saveit/SaveIt.java
@@ -2,6 +2,11 @@ package budget.saveit;
 
 import android.app.Application;
 
+import java.util.Date;
+
+import budget.saveit.model.OneTimeExpense;
+import budget.saveit.model.db.DB;
+
 /**
  * Created by aa on 12/06/17.
  */
@@ -10,5 +15,9 @@ public class SaveIt extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        DB db = new DB(getApplicationContext());
+        db.clearDB();
+        db.addOneTimeExpense(new OneTimeExpense(30, new Date()));
     }
 }
