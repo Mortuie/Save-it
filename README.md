b7fc2b4e89183f4f843a17f508a94daf70db67a0
mortuie <jimmybob-leon@hotmail.co.uk>
2017-06-15 22:27:40 +0100

    Added some small thing to the README.md

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Save it. A budgeting application for you
+
+
+
+### Authors
+
+2017 (C) Atul Anand & Leon Boehmer 
--- app/src/main/java/budget/saveit/view/LoginScreen.java
+++ app/src/main/java/budget/saveit/view/LoginScreen.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import budget.saveit.R;
@@ -34,7 +33,7 @@ public class LoginScreen extends Activity {
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
 
-        
+
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
